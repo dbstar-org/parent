c5a143ca764d4f3d2897250ed9f2ecb4bd516a1e
dbstar <dbstarll@gmail.com>
2021-07-16 17:54:04 +0800

    增加test项目，调整pure的插件定义

---
+++ test/src/main/java/io/github/dbstarll/parent/test/Starter.java
@@ -0,0 +1,7 @@
+package io.github.dbstarll.parent.test;
+
+public class Starter {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ test/src/test/java/io/github/dbstarll/parent/test/StarterTest.java
@@ -0,0 +1,11 @@
+package io.github.dbstarll.parent.test;
+
+public class StarterTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
